93837154ae5c22c4fd727a2b287295557b5a6abb
kumarananda <coderanandakumar@gmail.com>
2022-07-06 18:51:49 +0600

    basic

---
+++ react-hooks-p-01/src/components/pages/Page4.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Page4 = () => {
+  return (
+    <div>
+        <input type="text" value={'kkkk'} onChange={e => e.target.value} />
+    </div>
+  )
+}
+
+export default Page4
